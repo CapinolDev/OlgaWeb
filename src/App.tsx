7d9886c5f0e66dabafbe5c116e5ec5341c48f118
Erdinc Daskin <eruanio33@gmail.com>
2025-11-27 21:56:51 +0100

    added cool bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import "./App.css";
 import Navbar from "./components/navbar";
+import Silk from "./components/Silk";
 
 function App() {
   return (
     <div className="page">
-      <Navbar />
-      <main className="content"></main>
+      <div className="canvas-bg">
+        <Silk
+          className="silk-canvas"
+          color="#7B7481"
+          speed={6}
+          scale={1}
+          noiseIntensity={1.2}
+        />
+      </div>
+
+      <div className="page-shell">
+        <Navbar />
+        <main className="content"></main>
+      </div>
     </div>
   );
 }
