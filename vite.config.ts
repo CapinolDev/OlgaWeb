99c658dc463e6c7f0f58a4a6769082c005f08012
Erdinc Daskin <eruanio33@gmail.com>
2025-12-09 21:29:39 +0100

    FUCKING WORK

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/OlgaWeb',
-  build: {
-    outDir: 'docs' // neuk
-  }
+  base: '/OlgaWeb'
 });
