beb3be4eec71041554ee476d7d1e0a0de874bd38
Erdinc Daskin <eruanio33@gmail.com>
2025-12-09 21:50:00 +0100

    idk anymore

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter basename="/OlgaWeb">
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>
