787498afed3cbc466ccfe7c68dfc0fc7c73ba069
Erdinc Daskin <eruanio33@gmail.com>
2025-12-09 21:21:41 +0100

    dfsdf

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/OlgaWeb/',
+  base: '/OlgaWeb',
   build: {
     outDir: 'docs' // neuk
   }
